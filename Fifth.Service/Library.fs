6b42f648577b35a29170c3fdec72c5e1a1d861c4
ridecar2 <ridecar2@gmail.com>
2020-03-31 03:47:43 +0100

    Add some basic UI boilerplate.

---
+++ Fifth.Service/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Fifth.Service
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ Fifth.Web/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Ooui
+
+
+[<EntryPoint>]
+let main argv =
+
+    let button = Button("Click Me!")
+
+    let mutable count = 0
+    button.Click.Add(fun _ ->
+        count <- count + 1
+        button.Text <- (sprintf "Clicked %d times." count))
+    
+    // Required to run as a non-admin: https://github.com/praeclarum/Ooui/issues/152#issuecomment-419639448
+    UI.Host <- "localhost"
+    UI.Publish ("/", button)
+    UI.Present ("/")
+
+    Console.ReadLine () |> ignore
+    0 // return an integer exit code
